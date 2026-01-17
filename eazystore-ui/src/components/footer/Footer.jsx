4b2a7296007694a81ad89a67ab6e09f4fce20cb7
sikandarmalik <getintouchwithsik@gmail.com>
2026-01-17 18:41:44 +0800

    Installed TailwindCSS/Vite, cleared all index.css styling, deleted footer.css and added some background color to the html body with Tailwind css utility-class

--- eazystore-ui/src/components/footer/Footer.jsx
+++ eazystore-ui/src/components/footer/Footer.jsx
@@ -1,6 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import "./footer.css";
 
 export default function Footer() {
   return (
