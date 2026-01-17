f8f4783bd49fe54f2c8979d99452686f01a5a40c
sikandarmalik <getintouchwithsik@gmail.com>
2026-01-17 17:00:53 +0800

    Built ProductListings, ProductCard, Price Functional Components, Imported Sticker images to assets folder, setup products mock data and displayed in Home Page of product image, name, description and price

--- eazystore-ui/src/components/Home.jsx
+++ eazystore-ui/src/components/Home.jsx
@@ -1,4 +1,6 @@
+import products from "../data/products";
 import PageHeading from "./PageHeading";
+import ProductListings from "./ProductListings";
 
 export default function Home() {
   return (
@@ -7,6 +9,7 @@ export default function Home() {
         Add a touch of creativity to your space with our wide range of fun and
         unique stickers. Perfect for any occasion!
       </PageHeading>
+      <ProductListings products={products} />
     </div>
   );
 }
---
+++ eazystore-ui/src/components/ProductListings.jsx
@@ -0,0 +1,18 @@
+import ProductCard from "./ProductCard";
+
+export default function ProductListings({ products }) {
+  return (
+    <div className="product-listings-container">
+      <div className="product-listings-grid">
+        {/* Dummy product logic */}
+        {products.length > 0 ? (
+          products.map((product) => (
+            <ProductCard key={product.productId} product={product} />
+          ))
+        ) : (
+          <p className="product-listings-empty">No products found</p>
+        )}
+      </div>
+    </div>
+  );
+}
