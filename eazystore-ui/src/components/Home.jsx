9662b60462881881144d59d1219d88e7fb192a1c
sikandarmalik <getintouchwithsik@gmail.com>
2026-01-18 17:16:02 +0800

    Implemented tailwind css class-utility to Home Page layout, Page Heaing, Page Title, Product Listings and Product Card.

--- eazystore-ui/src/components/Home.jsx
+++ eazystore-ui/src/components/Home.jsx
@@ -4,7 +4,7 @@ import ProductListings from "./ProductListings";
 
 export default function Home() {
   return (
-    <div className="home-container">
+    <div className="max-w-6xl mx-auto px-6 py-8">
       <PageHeading title="Explore Eazy Stickers!">
         Add a touch of creativity to your space with our wide range of fun and
         unique stickers. Perfect for any occasion!
--- eazystore-ui/src/components/ProductListings.jsx
+++ eazystore-ui/src/components/ProductListings.jsx
@@ -2,15 +2,17 @@ import ProductCard from "./ProductCard";
 
 export default function ProductListings({ products }) {
   return (
-    <div className="product-listings-container">
-      <div className="product-listings-grid">
+    <div className="max-w-6xl mx-auto">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-y-8 gap-x-6 py-12">
         {/* Dummy product logic */}
         {products.length > 0 ? (
           products.map((product) => (
             <ProductCard key={product.productId} product={product} />
           ))
         ) : (
-          <p className="product-listings-empty">No products found</p>
+          <p className="text-center font-primary font-bold text-lg text-primary">
+            No products found
+          </p>
         )}
       </div>
     </div>
